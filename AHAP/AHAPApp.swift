c222cbc4b9e184d96423c0302d9d5efea2fc2502
Henrik Storch <henrik@getsquareone.app>
2024-04-09 15:58:56 +1200

    Initial Commit

---
+++ AHAP/AHAPApp.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2024 SquareOne. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AHAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
